60df1593b350617fd963cb018a8181691b420c83
SyndraMaster <100042275+SyndraMaster@users.noreply.github.com>
2022-04-23 09:01:34 -0500

    Add files via upload
    Terminado frontend, iniciando lógica

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-
-
-// ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
 let clicked = null;
 let fecha = new Date()
 let contenedor = null;
 let turnSelect = null;
+let dia = null;
+let calendario = [];
+let modal = document.querySelector('.modal');
+let cerrar = document.querySelector('.cerrar');
+let turno = document.querySelector('#turno');
 
 function generarCalendario () {
     fecha.setDate(1)
@@ -18,7 +21,7 @@ function generarCalendario () {
     let mes = document.querySelector('.mes')
     let meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
     let diasMes = document.querySelector('.dias-mes');
-    mes.innerText = `${meses[fecha.getMonth()]}`
+    mes.innerText = `${meses[fecha.getMonth()]} ${anoActual}`
     let days = '';
     for (let x = indexPrimerDia; x > 0; x--) {
         days += `<div class='prev-days'><p>${ultDiaMesPrevio - x + 1}</p></div>`;
@@ -31,6 +34,11 @@ function generarCalendario () {
         days += `<div class='post-days'><p>${j + 1}</p><div class="day-container"></div></div>`;
         diasMes.innerHTML = days;
     }
+    let dtFecha = {
+        mes: mesActual + 1,
+        ano: anoActual
+    }
+    crEvent(dtFecha);
 }
 document.querySelector('.previo').addEventListener('click', () => {
     fecha.setMonth(fecha.getMonth() - 1);
@@ -40,44 +48,63 @@ document.querySelector('.posterior').addEventListener('click', () => {
     fecha.setMonth(fecha.getMonth() + 1);
     generarCalendario();
 })
-generarCalendario();
 
-function crEvent () {
+function crEvent (dtFecha) {
     let dia = document.querySelectorAll('.day')
     for (let i = 0; i < dia.length; i++) {
         dia[i].addEventListener('click', modal)
     }
     function modal () {
         clicked = this;
-        evento(clicked);
+        evento(clicked, dtFecha);
     }
 }
 
-function evento (clicked) {
+function evento (clicked, dtFecha) {
     contenedor = clicked.lastChild;
+    dia = clicked.firstChild.textContent;
+    dtFecha['dia'] = Number(dia);
+    let diafechado = [dtFecha['ano'], dtFecha['mes'], dtFecha['dia']]
     contenedor.style.backgroundColor = "blue";
-    openModal(contenedor);
-    console.log(contenedor);
+    openModal();
+    calendario.push(empujarCalend(diafechado));
+    console.log(calendario)
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////
-function openModal (contenedor) {
-    
+function empujarCalend (diafechado) {
+    return diafechado;
 }
-crEvent();
-let turno = document.querySelector('#turno');
-turno.addEventListener('change', () => {
-    console.log(turno.value);
-})
+
+/////////////////////////////////////////////////////////////////
 let seleccionTurno = document.getElementsByClassName('pre-turno');
-for (let i = 0; i < seleccionTurno.length; i++) {
-    seleccionTurno[i].addEventListener('click', turnInit)
+function openModal () {
+    modal.classList.add("modalOpen");
+    for (let i = 0; i < seleccionTurno.length; i++) {
+        seleccionTurno[i].addEventListener('click', turnInit)
+    }
+    cerrar.addEventListener('click', () => {
+        modal.classList.remove('modalOpen');
+    })
 }
+
 function turnInit () {
     turnSelect = this;
     changeTurn(turnSelect);
 }
+
+
 function changeTurn (turnSelect) {
     turno.value = turnSelect.textContent;
-    console.log(turnSelect.textContent)
+    contenedor.textContent = turno.value;
 }
+
+
+// function guardarTurno () {
+    
+    // }
+    
+    
+    // function fechaActual () {
+        //     return
+        // }
+generarCalendario();
